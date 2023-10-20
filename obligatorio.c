e8a417a2067efc09ff69bfd3a03ff57bd45220c9
Tomás Spoturno <spoturno16@gmail.com>
2023-10-19 21:38:43 -0300

    fix: arreglo corchetes y simplifico condicion en C

--- obligatorio.c
+++ obligatorio.c
@@ -37,102 +37,93 @@ ModoAlmacenamiento modo_actual;
 short arbol[AREA_MEMORIA];
 unsigned short index_siguiente = 0;
 
-int main()
-{
+int main() {
     short comando, parametro;
 
-    while (1)
-    {
+    while (1) {
         // Leer comando del PUERTO_ENTRADA
         comando = in(PUERTO_ENTRADA);
         out(PUERTO_LOG, 64);
         out(PUERTO_LOG, comando);
 
-        switch (comando)
-        {
-        case 1: // Cambiar Modo
-            parametro = in(PUERTO_ENTRADA);
-            out(PUERTO_LOG, parametro);
-            if (parametro == MODO_ESTATICO || parametro == MODO_DINAMICO)
-            {
-                cambiarModo(parametro);
+        switch (comando) {
+            case 1: // Cambiar Modo
+                parametro = in(PUERTO_ENTRADA);
+                out(PUERTO_LOG, parametro);
+                if (parametro == MODO_ESTATICO) {
+                    cambiarModo(parametro);
+                    out(PUERTO_LOG, 0);
+                } else if (parametro == MODO_DINAMICO) {
+                    cambiarModo(parametro);
+                    out(PUERTO_LOG, 0);
+                } else {
+                    out(PUERTO_LOG, 2); // Parámetro inválido
+                }
+                break;
+
+            case 2: // Agregar Nodo
+                parametro = in(PUERTO_ENTRADA);
+                out(PUERTO_LOG, parametro);
+                insertar(parametro);
                 out(PUERTO_LOG, 0);
-            }
-            else
-            {
-                out(PUERTO_LOG, 2); // Parámetro inválido
-            }
-            break;
+                break;
 
-        case 2: // Agregar Nodo
-            parametro = in(PUERTO_ENTRADA);
-            out(PUERTO_LOG, parametro);
-            insertar(parametro);
-            out(PUERTO_LOG, 0);
-            break;
+            case 3: // Calcular Altura
+                out(PUERTO_SALIDA, altura());
+                out(PUERTO_LOG, 0);
+                break;
 
-        case 3: // Calcular Altura
-            out(PUERTO_SALIDA, altura());
-            out(PUERTO_LOG, 0);
-            break;
+            case 4: // Calcular Suma
+                out(PUERTO_SALIDA, suma());
+                out(PUERTO_LOG, 0);
+                break;
 
-        case 4: // Calcular Suma
-            out(PUERTO_SALIDA, suma());
-            out(PUERTO_LOG, 0);
-            break;
+            case 5: // Imprimir Árbol
+                parametro = in(PUERTO_ENTRADA);
+                out(PUERTO_LOG, parametro);
+                imprimir(parametro);
+                out(PUERTO_LOG, 0);
+                break;
 
-        case 5: // Imprimir Árbol
-            parametro = in(PUERTO_ENTRADA);
-            out(PUERTO_LOG, parametro);
-            imprimir(parametro);
-            out(PUERTO_LOG, 0);
-            break;
+            case 6: // Imprimir Memoria
+                parametro = in(PUERTO_ENTRADA);
+                out(PUERTO_LOG, parametro);
+                // TODO: imprimirMemoria(parametro)
+                out(PUERTO_LOG, 0);
+                break;
 
-        case 6: // Imprimir Memoria
-            parametro = in(PUERTO_ENTRADA);
-            out(PUERTO_LOG, parametro);
-            // Aquí necesitarías una función para imprimir la memoria
-            // Lo he dejado sin implementar, pero deberías añadir dicha función basándote en el enunciado.
-            out(PUERTO_LOG, 0);
-            break;
+            case 255: // Detener programa
+                out(PUERTO_LOG, 0);
+                return 0;
 
-        case 255: // Detener programa
-            out(PUERTO_LOG, 0);
-            return 0;
-
-        default:
-            out(PUERTO_LOG, 1); // Comando inválido
-            break;
+            default:
+                out(PUERTO_LOG, 1); // Comando inválido
+                break;
         }
     }
 
     return 0;
 }
 
-void inicializarMemoria()
-{
-    for (unsigned short i = 0; i < AREA_MEMORIA; i++)
-    {
+void inicializarMemoria() {
+    for (unsigned short i = 0; i < AREA_MEMORIA; i++) {
         arbol[i] = NODO_VACIO;
     }
     index_siguiente = 0;
 }
 
-void cambiarModo(ModoAlmacenamiento modo)
-{
+void cambiarModo(ModoAlmacenamiento modo) {
     modo_actual = modo;
     inicializarMemoria();
 }
 
-short in(unsigned short puerto)
-{
+short in(unsigned short puerto) {
     short valor;
     scanf("%d", &valor);
     return valor;
 }
 
-void out(unsigned short puerto, short valor)
-{
+void out(unsigned short puerto, short valor) {
     printf("%d%d", puerto, valor);
 }
 
@@ -186,39 +177,63 @@ void insertarDinamico(unsigned short index, short num) {
 }
 
 unsigned short alturaEstatico(unsigned short index) {
-    if (index >= AREA_MEMORIA || arbol[index] == NODO_VACIO) {
+    if (index >= AREA_MEMORIA) {
+        return 0;
+    }
+
+    if (arbol[index] == NODO_VACIO){
         return 0;
     }
 
     unsigned short izq = alturaEstatico(2 * index + 1);
     unsigned short der = alturaEstatico(2 * index + 2);
 
-    return 1 + (izq > der ? izq : der);
+    if (izq > der) {
+        return 1 + izq;
+    } else {
+        return 1 + der;
+    }
 }
 
-unsigned short alturaDinamico(unsigned short index)
-{
-    if (3 * index >= AREA_MEMORIA || arbol[3 * index] == NODO_VACIO)
+unsigned short alturaDinamico(unsigned short index) {
+    if (3 * index >= AREA_MEMORIA) {
         return 0;
+    }
+
+    if (arbol[3 * index] == NODO_VACIO) {
+        return 0;
+    }
 
     unsigned short izq = arbol[3 * index + 1] != NODO_VACIO ? alturaDinamico(arbol[3 * index + 1]) : 0;
     unsigned short der = arbol[3 * index + 2] != NODO_VACIO ? alturaDinamico(arbol[3 * index + 2]) : 0;
 
-    return 1 + (izq > der ? izq : der);
+    if (izq > der) {
+        return 1 + izq;
+    } else {
+        return 1 + der;
+    }
 }
 
-unsigned short sumaEstatico(unsigned short index)
-{
-    if (index >= AREA_MEMORIA || arbol[index] == NODO_VACIO)
+unsigned short sumaEstatico(unsigned short index) {
+    if (index >= AREA_MEMORIA){
         return 0;
+    }
+
+    if (arbol[index] == NODO_VACIO) {
+        return 0;
+    }
 
     return arbol[index] + sumaEstatico(2 * index + 1) + sumaEstatico(2 * index + 2);
 }
 
-unsigned short sumaDinamico(unsigned short index)
-{
-    if (3 * index >= AREA_MEMORIA || arbol[3 * index] == NODO_VACIO)
+unsigned short sumaDinamico(unsigned short index) {
+    if (3 * index >= AREA_MEMORIA) {
         return 0;
+    }
+
+    if (arbol[3 * index] == NODO_VACIO) {
+        return 0;
+    }
 
     unsigned short sumIzq = arbol[3 * index + 1] != NODO_VACIO ? sumaDinamico(arbol[3 * index + 1]) : 0;
     unsigned short sumDer = arbol[3 * index + 2] != NODO_VACIO ? sumaDinamico(arbol[3 * index + 2]) : 0;
@@ -226,94 +241,89 @@ unsigned short sumaDinamico(unsigned short index)
     return arbol[3 * index] + sumIzq + sumDer;
 }
 
-void imprimirEstatico(unsigned short index, int orden)
-{
-    if (index >= AREA_MEMORIA || arbol[index] == NODO_VACIO)
+void imprimirEstatico(unsigned short index, int orden) {
+    if (index >= AREA_MEMORIA) {
         return;
+    }
 
-    if (orden == 0)
-    {
+    if(arbol[index] == NODO_VACIO) {
+        return;
+    }
+
+    if (orden == 0) {
         imprimirEstatico(2 * index + 1, orden);
         printf("%d ", arbol[index]);
         imprimirEstatico(2 * index + 2, orden);
-    }
-    else
-    {
+    } else {
         imprimirEstatico(2 * index + 2, orden);
         printf("%d ", arbol[index]);
         imprimirEstatico(2 * index + 1, orden);
     }
 }
 
-void imprimirDinamico(unsigned short index, int orden)
-{
-    if (3 * index >= AREA_MEMORIA || arbol[3 * index] == NODO_VACIO)
+void imprimirDinamico(unsigned short index, int orden) {
+    if (3 * index >= AREA_MEMORIA) {
         return;
-
-    if (orden == 0)
-    {
-        if (arbol[3 * index + 1] != NODO_VACIO)
-            imprimirDinamico(arbol[3 * index + 1], orden);
-        printf("%d ", arbol[3 * index]);
-        if (arbol[3 * index + 2] != NODO_VACIO)
-            imprimirDinamico(arbol[3 * index + 2], orden);
     }
-    else
-    {
-        if (arbol[3 * index + 2] != NODO_VACIO)
-            imprimirDinamico(arbol[3 * index + 2], orden);
-        printf("%d ", arbol[3 * index]);
-        if (arbol[3 * index + 1] != NODO_VACIO)
+
+    if (arbol[3 * index] == NODO_VACIO) {
+        return;
+    }
+
+    if (orden == 0) {
+        if (arbol[3 * index + 1] != NODO_VACIO) {
             imprimirDinamico(arbol[3 * index + 1], orden);
+        }
+
+        printf("%d ", arbol[3 * index]);
+
+        if (arbol[3 * index + 2] != NODO_VACIO) {
+            imprimirDinamico(arbol[3 * index + 2], orden);
+        }
+            
+    } else {
+        if (arbol[3 * index + 2] != NODO_VACIO) {
+            imprimirDinamico(arbol[3 * index + 2], orden);
+        }
+
+        printf("%d ", arbol[3 * index]);
+
+        if (arbol[3 * index + 1] != NODO_VACIO) {
+            imprimirDinamico(arbol[3 * index + 1], orden);
+        }
     }
 }
 
 /* Funciones Principales */
 
-void insertar(short num)
-{
-    if (modo_actual == MODO_ESTATICO)
-    {
+void insertar(short num) {
+    if (modo_actual == MODO_ESTATICO) {
         insertarEstatico(0, num);
-    }
-    else
-    {
+    } else {
         insertarDinamico(0, num);
     }
 }
 
-unsigned short altura()
-{
-    if (modo_actual == MODO_ESTATICO)
-    {
+unsigned short altura() {
+    if (modo_actual == MODO_ESTATICO) {
         return alturaEstatico(0);
-    }
-    else
-    {
+    } else {
         return alturaDinamico(0);
     }
 }
 
-unsigned short suma()
-{
-    if (modo_actual == MODO_ESTATICO)
-    {
+unsigned short suma() {
+    if (modo_actual == MODO_ESTATICO) {
         return sumaEstatico(0);
-    }
-    else
-    {
+    } else {
         return sumaDinamico(0);
     }
 }
 
-void imprimir(int orden)
-{
-    if (modo_actual == MODO_ESTATICO)
-    {
+void imprimir(int orden) {
+    if (modo_actual == MODO_ESTATICO) {
         imprimirEstatico(0, orden);
-    }
-    else
-    {
+    } else {
         imprimirDinamico(0, orden);
     }
     printf("\n");
